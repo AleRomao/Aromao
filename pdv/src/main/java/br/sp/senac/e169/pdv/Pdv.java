aa8cd0c5bb846549b4fb9295f0e25957272a4544
AleRomao <129014981+AleRomao@users.noreply.github.com>
2023-10-13 11:24:56 -0300

    Add files via upload

---
+++ pdv/src/main/java/br/sp/senac/e169/pdv/Pdv.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.sp.senac.e169.pdv;
+
+/**
+ *
+ * @author romao
+ */
+public class Pdv {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
